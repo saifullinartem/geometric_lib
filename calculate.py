e6c2c9ae3d38195b6adba7af2cd1abf32463d818
NEPIPENKO EGOR <466859@niuitmo.ru>
2024-11-17 17:39:53 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,40 +1,15 @@
-import circle
-import square
-import triangle
 
+class Calculator:
+		def add(self, a, b):
+				return a + b
 
-figs = ['circle', 'square', 'triangle']
-funcs = ['perimeter', 'area']
-sizes = {
-		"area-circle": 1,
-		"perimeter-circle": 1,
-		"area-square": 1,
-		"perimeter-square": 1,
-		"area-triangle": 3,
-		"perimeter-triangle": 3
-}
+		def subtract(self, a, b):
+				return a - b
 
+		def multiply(self, a, b):
+				return a * b
 
-def calc(fig, func, size):
-		assert fig in figs, f"Invalid figure: {fig}. Allowed values are {figs}."
-		assert func in funcs, f"Invalid function: {func}. Allowed values are {funcs}."
-		assert len(size) == sizes.get(f"{func}-{fig}", 1), f"Invalid number of parameters for {func} of {fig}."
-		result = eval(f'{fig}.{func}(*{size})')
-		return result
-
-
-if __name__ == "main":
-		func = ''
-		fig = ''
-		size = list()
-
-		while fig not in figs:
-				fig = input(f"Enter figure name, available are {figs}:\n")
-
-		while func not in funcs:
-				func = input(f"Enter function name, available are {funcs}:\n")
-
-		while len(size) != sizes.get(f"{func}-{fig}", 1):
-				size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-
-		print(calc(fig, func, size))
+		def divide(self, a, b):
+				if b == 0:
+						raise ValueError("Cannot divide by zero.")
+				return a / b
