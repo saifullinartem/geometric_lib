eb7ddcea33af06edab9fc6f2c9c1ea454697a8bd
Artem Saifullin <467332@niuitmo.ru>
2024-11-22 17:06:49 +0300

    суьыщауым

--- geometric_lib/calculate.py
+++ geometric_lib/calculate.py
@@ -1,42 +1,55 @@
-# geometric_lib/calculate.py
+from circle import area as circle_area, perimeter as circle_perimeter
+from square import area as square_area, perimeter as square_perimeter
+from triangle import area as triangle_area, perimeter as triangle_perimeter
 
-from typing import Dict
 
-def calculate_area(radius: float) -> float:
-    """
-    Вычисляет площадь круга по заданному радиусу.
-    
-    :param radius: Радиус круга
-    :return: Площадь круга
-    """
-    area = 3.14 * radius ** 2
-    return area
+figs = ['circle', 'square', 'triangle']
+funcs = ['perimeter', 'area']
 
-def calculate_perimeter(radius: float) -> float:
-    """
-    Вычисляет периметр (длину окружности) круга по заданному радиусу.
-    
-    :param radius: Радиус круга
-    :return: Периметр круга
-    """
-    perimeter = 2 * 3.14 * radius
-    return perimeter
+figures = {
+    'circle': {'area': circle_area, 'perimeter': circle_perimeter},
+    'square': {'area': square_area, 'perimeter': square_perimeter},
+    'triangle': {'area': triangle_area, 'perimeter': triangle_perimeter}
+}
 
-def create_shape_dict(name: str, radius: float) -> Dict[str, float]:
-    """
-    Создаёт словарь с информацией о форме.
-    
-    :param name: Название формы
-    :param radius: Радиус формы
-    :return: Словарь с ключами 'name' и 'radius'
-    """
-    return {'name': name, 'radius': radius}
+sizes = {
+    'circle-area': 1,
+    'circle-perimeter': 1,
+    'square-area': 1,
+    'square-perimeter': 1,
+    'triangle-area': 3,
+    'triangle-perimeter': 3
+}
 
-def update_shape_radius(shape: Dict[str, float], new_radius: float) -> None:
-    """
-    Обновляет радиус формы в словаре.
-    
-    :param shape: Словарь с информацией о форме
-    :param new_radius: Новый радиус формы
-    """
-    shape['radius'] = new_radius
+
+def calc(fig, func, size):
+    assert fig in figs, f"Unknown figure: {fig}"
+    assert func in funcs, f"Unknown function: {func}"
+
+    key = f'{fig}-{func}'
+    assert len(size) == sizes[key], f"Invalid size for {fig} {func}"
+
+    return figures[fig][func](*size)
+
+
+if __name__ == "__main__":
+    func = ''
+    fig = ''
+    size = []
+
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {figs}: \n")
+
+    while func not in funcs:
+        func = input(f"Enter function name, available are {funcs}: \n")
+
+    while len(size) != sizes.get(f"{fig}-{func}", 1):
+        size = list(
+            map(
+                int,
+                input("Input figure sizes separated by space\n").split()
+            )
+        )
+
+    result = calc(fig, func, size)
+    print(f"Result: {result}")
