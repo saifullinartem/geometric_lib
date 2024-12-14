d1a9188c3e401214009f7a96c73e59a97393d0c2
Artem Saifullin <467332@niuitmo.ru>
2024-12-14 12:26:19 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,7 +1,7 @@
 import math
-from .circle import area as circle_area, perimeter as circle_perimeter
-from .square import area as square_area, perimeter as square_perimeter
-from .triangle import area as triangle_area, perimeter as triangle_perimeter
+from circle import area as circle_area, perimeter as circle_perimeter
+from square import area as square_area, perimeter as square_perimeter
+from triangle import area as triangle_area, perimeter as triangle_perimeter
 
 def calculate_area(radius: float) -> float:
     """
